86c86fbf21cd13917d150c03ac34dee0ca97c9f8
SECHET Brandon <brandon.sechet@partnre.com>
2022-04-12 09:29:03 +0200

    feat(app): create url validator for the form

---
+++ bot/admin/web/src/app/shared/validators/customsValidators.validators.ts
@@ -0,0 +1,11 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export class CustomsValidators {
+  static url(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    const pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+    const urlRegex = new RegExp(pattern);
+
+    return urlRegex.test(value) ? null : { url: true };
+  }
+}
