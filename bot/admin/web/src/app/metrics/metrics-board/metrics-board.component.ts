94ee9aac29f7b9907da8d99b78b748f8beb3b992
rkuffer <rodolphe.kuffer@partnre.com>
2023-04-07 16:09:31 +0200

    Metrics board

--- bot/admin/web/src/app/metrics/metrics-board/metrics-board.component.ts
+++ bot/admin/web/src/app/metrics/metrics-board/metrics-board.component.ts
@@ -1,121 +1,360 @@
-import { Component, OnDestroy } from '@angular/core';
-import { NbCalendarRange, NbDateService } from '@nebular/theme';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NbCalendarRange, NbDatepickerDirective, NbDateService } from '@nebular/theme';
 import type { EChartsOption } from 'echarts';
-import { Subject } from 'rxjs';
+import { forkJoin, Observable, Subject, take, takeUntil } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { AnalyticsService } from '../../analytics/analytics.service';
+import { DialogFlowRequest } from '../../analytics/flow/flow';
+import { UserAnalyticsQueryResult } from '../../analytics/users/users';
+import { RestService } from '../../core-nlp/rest/rest.service';
+import { StateService } from '../../core-nlp/state.service';
+import { BotConfigurationService } from '../../core/bot-configuration.service';
+import { BotApplicationConfiguration } from '../../core/model/configuration';
+import { IndicatorDefinition, MetricResult, StorySummary } from '../models';
 
+export enum TimeRanges {
+  day = 1,
+  week = 7,
+  month = 31,
+  quarter = 92
+}
 @Component({
   selector: 'tock-metrics-board',
   templateUrl: './metrics-board.component.html',
   styleUrls: ['./metrics-board.component.scss']
 })
-export class MetricsBoardComponent implements OnDestroy {
+export class MetricsBoardComponent implements OnInit, OnDestroy {
   destroy = new Subject();
-
+  loading: boolean = true;
+  configurations: BotApplicationConfiguration[];
+  indicators: IndicatorDefinition[];
+  stories: StorySummary[];
   range: NbCalendarRange<Date>;
+  TimeRanges = TimeRanges;
 
-  constructor(protected dateService: NbDateService<Date>) {
+  @ViewChild(NbDatepickerDirective) dateRangeInputDirectiveRef;
+
+  constructor(
+    private stateService: StateService,
+    protected dateService: NbDateService<Date>,
+    private analyticsService: AnalyticsService,
+    private botConfiguration: BotConfigurationService,
+    private rest: RestService
+  ) {
     this.range = {
-      start: this.dateService.addDay(this.monthStart, 3),
-      end: this.dateService.addDay(this.monthEnd, -3)
+      start: this.dateService.addDay(this.dateService.today(), -TimeRanges.week),
+      end: this.dateService.today()
     };
   }
 
-  get monthStart(): Date {
-    return this.dateService.getMonthStart(new Date());
+  ngOnInit(): void {
+    this.loading = true;
+    this.botConfiguration.configurations.pipe(takeUntil(this.destroy)).subscribe((confs) => {
+      this.configurations = confs;
+      if (confs.length) {
+        this.loadIndicatorsAndStories();
+      }
+    });
   }
 
-  get monthEnd(): Date {
-    return this.dateService.getMonthEnd(new Date());
+  private loadIndicatorsAndStories(): void {
+    const loaders = [this.getIndicatorsQuery().pipe(take(1)), this.getStoriesSummaryQuery().pipe(take(1))];
+
+    forkJoin(loaders).subscribe(([indicators, stories]: [IndicatorDefinition[], StorySummary[]]) => {
+      this.indicators = indicators;
+      this.stories = stories;
+      this.initCurrentDimension();
+      this.loadMetrics();
+    });
   }
 
-  queriesStatsData = [
-    ['2023-01-27', 116],
-    ['2023-01-28', 129],
-    ['2023-01-29', 135],
-    ['2023-01-30', 86],
-    ['2023-01-31', 73],
-    ['2023-02-01', 85],
-    ['2023-02-02', 185],
-    ['2023-02-03', 105]
-  ];
+  private getIndicatorsQuery(): Observable<IndicatorDefinition[]> {
+    const url = `/bot/${this.stateService.currentApplication.name}/indicators`;
+    return this.rest.get(url, (indicators) => indicators);
+  }
 
-  queriesStatsDateList = this.queriesStatsData.map(function (item) {
-    return item[0];
-  });
-  queriesStatsValueList = this.queriesStatsData.map(function (item) {
-    return item[1];
-  });
+  private getStoriesSummaryQuery(): Observable<StorySummary[]> {
+    const url = `/bot/story/search/summary`;
+    const payload = {
+      applicationName: this.stateService.currentApplication.name,
+      namespace: this.stateService.currentApplication.namespace
+    };
+    return this.rest.post(url, payload);
+  }
 
-  queriesStats: EChartsOption = {
-    xAxis: {
-      data: this.queriesStatsDateList
-    },
-    yAxis: {
-      type: 'value'
-    },
-    series: [
-      {
-        type: 'line',
-        smooth: true,
-        data: this.queriesStatsValueList
+  private loadMetrics(): void {
+    this.loading = true;
+    const loaders = [
+      this.getMessagesSearchQuery().pipe(take(1)),
+      this.getStoriesHitsQuery().pipe(take(1)),
+      this.getCurrentDimensionMetricsQuery().pipe(take(1))
+    ];
+
+    forkJoin(loaders).subscribe(([messages, storiesHits, dimensionMetrics]: [UserAnalyticsQueryResult, MetricResult[], MetricResult[]]) => {
+      this.initMessagesChart(messages);
+      this.storiesMetrics = storiesHits;
+      this.initStoriesHitsChart();
+      this.initCurrentDimensionMetricsChart(dimensionMetrics);
+      this.loading = false;
+    });
+  }
+
+  private loadCurrentDimensionMetrics(): void {
+    this.loading = true;
+    this.getCurrentDimensionMetricsQuery()
+      .pipe(take(1))
+      .subscribe((dimensionMetrics) => {
+        this.initCurrentDimensionMetricsChart(dimensionMetrics);
+        this.loading = false;
+      });
+  }
+
+  private getMessagesSearchQuery(): Observable<UserAnalyticsQueryResult> {
+    return this.analyticsService.messagesAnalytics(
+      new DialogFlowRequest(
+        this.stateService.currentApplication.namespace,
+        this.stateService.currentApplication.name,
+        this.stateService.currentLocale,
+        this.stateService.currentApplication.name,
+        undefined,
+        undefined,
+        this.range.start,
+        this.range.end,
+        !environment.production // In dev we ask for test messages to have usable content
+      )
+    );
+  }
+
+  private getStoriesHitsQuery(): Observable<MetricResult[]> {
+    const query = {
+      filter: {
+        types: ['STORY_HANDLED'],
+        creationDateSince: this.range.start,
+        creationDateUntil: this.range.end
+      },
+      groupBy: ['TRACKED_STORY_ID']
+    };
+    const url = `/bot/${this.stateService.currentApplication.name}/metrics`;
+    return this.rest.post(url, query);
+  }
+
+  private messagesStatsData: UserAnalyticsQueryResult;
+  messagesChartOptions: EChartsOption;
+
+  private initMessagesChart(rawStats: UserAnalyticsQueryResult): void {
+    this.messagesStatsData = rawStats;
+
+    this.messagesChartOptions = {
+      tooltip: {
+        trigger: 'axis',
+        formatter: function (params) {
+          const plural = params[0].data > 0 ? 's' : '';
+          return `${params[0].name}<br />${params[0].data} message${plural}`;
+        }
+      },
+      xAxis: {
+        data: rawStats.dates as any
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          type: 'line',
+          smooth: true,
+          areaStyle: {},
+          data: rawStats.usersData.map((val) => val[0])
+        }
+      ]
+    };
+  }
+
+  private storiesMetrics: MetricResult[];
+  storiesChart: EChartsOption;
+
+  private initStoriesHitsChart(): void {
+    this.storiesChart = {
+      tooltip: {
+        trigger: 'item',
+        formatter: function (params) {
+          const plural = params.data.value > 1 ? 's' : '';
+          return `Story <strong>${params.data.name}</strong> <br />was triggered <strong>${params.data.value} time${plural}</strong> (${params.percent}%)`;
+        }
+      },
+      calculable: true,
+      series: [
+        {
+          name: 'stories',
+          type: 'pie',
+          radius: [30, 110],
+          roseType: 'area',
+          itemStyle: {
+            borderRadius: 4
+          },
+          data: this.storiesMetrics
+            .map((hit) => {
+              return {
+                value: hit.count,
+                name: this.getStoryNameById(hit.row.trackedStoryId)
+              };
+            })
+            .sort((a, b) => {
+              return a.value - b.value;
+            })
+        }
+      ]
+    };
+  }
+
+  currentDimension: string;
+
+  private initCurrentDimension(): void {
+    this.currentDimension = this.indicatorsDimensions[0];
+  }
+
+  dimensionSelected(dimension): void {
+    this.currentDimension = dimension;
+    this.loadCurrentDimensionMetrics();
+  }
+
+  get currentDimensionIndicators(): IndicatorDefinition[] {
+    return this.indicators.filter((indicator) => indicator.dimensions.includes(this.currentDimension));
+  }
+
+  private getCurrentDimensionMetricsQuery(): Observable<MetricResult[]> {
+    const dimensionIndicatorsNames = this.currentDimensionIndicators.map((indicator) => indicator.name);
+    const query = {
+      filter: {
+        indicatorNames: dimensionIndicatorsNames,
+        creationDateSince: this.range.start,
+        creationDateUntil: this.range.end
+      },
+      groupBy: ['TYPE', 'INDICATOR_NAME', 'INDICATOR_VALUE_NAME']
+    };
+    const url = `/bot/${this.stateService.currentApplication.name}/metrics`;
+    return this.rest.post(url, query);
+  }
+
+  currentDimensionCharts: EChartsOption[];
+
+  private initCurrentDimensionMetricsChart(dimensionMetrics: MetricResult[]): void {
+    this.currentDimensionCharts = [];
+
+    this.currentDimensionIndicators.forEach((indicator) => {
+      const entries = [];
+
+      const indicatorLabel = this.getIndicatorLabelByName(indicator.name);
+
+      const indicatorMetrics = dimensionMetrics.filter((dimMetric) => dimMetric.row.indicatorName === indicator.name);
+
+      const indicatorMetricsReplies = indicatorMetrics.filter((indMetric) => indMetric.row.type === 'QUESTION_REPLIED');
+
+      let repliesCount = 0;
+
+      indicatorMetricsReplies.forEach((imr) => {
+        entries.push({
+          value: imr.count,
+          name: this.getIndicatorValueLabelByName(imr.row.indicatorName, imr.row.indicatorValueName)
+        });
+        repliesCount += imr.count;
+      });
+
+      let indicatorMetricsQuestion = indicatorMetrics.find((indMetric) => indMetric.row.type === 'QUESTION_ASKED');
+      if (indicatorMetricsQuestion) {
+        const conversionRate = indicatorMetricsQuestion.count - repliesCount;
+        if (conversionRate) {
+          entries.push({
+            value: conversionRate,
+            name: 'No answer given',
+            itemStyle: { color: '#aaa' }
+          });
+        }
       }
-    ]
-  };
 
-  storiesStats: EChartsOption = {
-    tooltip: {
-      trigger: 'item',
-      formatter: '{c} ({d}%)'
-    },
-    calculable: true,
-    series: [
-      {
-        name: 'stories',
-        type: 'pie',
-        radius: [30, 110],
-        data: [
-          { value: 9, name: 'Prêt travaux' },
-          { value: 4, name: 'Apple pay' },
-          { value: 3, name: 'Faire une réclamation' },
-          { value: 2, name: 'Opposition sur carte/chéquier' },
-          { value: 1, name: 'Code de sécurité' },
-          { value: 1, name: 'Aide à la connexion' },
-          { value: 1, name: 'Identifiant de connexion' },
-          { value: 1, name: 'Gestion du mot de passe' },
-          { value: 1, name: 'Problème de connexion' },
-          { value: 1, name: 'Informations sur la localisation' },
-          { value: 1, name: 'Problème de connexion' },
-          { value: 1, name: 'Gestion du mot de passe' },
-          { value: 1, name: 'Code de sécurité' },
-          { value: 1, name: 'Identifiant de connexion' },
-          { value: 0, name: 'Aide à la connexion' },
-          { value: 1, name: 'Virement' }
+      this.currentDimensionCharts.push({
+        name: indicatorLabel,
+        tooltip: {
+          trigger: 'item',
+          formatter: function (params) {
+            return `${params.data.name} :<br /><strong>${params.percent}%</strong> (${params.data.value})`;
+          }
+        },
+        calculable: true,
+        series: [
+          {
+            name: 'dimension metrics',
+            type: 'pie',
+            radius: [60, 110],
+            itemStyle: {
+              borderRadius: 4
+            },
+            data: entries
+          }
         ]
-      }
-    ]
-  };
+      });
+    });
+  }
 
-  satisfactionStats: EChartsOption = {
-    tooltip: {
-      trigger: 'item',
-      formatter: '{c} ({d}%)'
-    },
-    calculable: true,
-    series: [
-      {
-        name: 'satisfaction',
-        type: 'pie',
-        radius: [30, 110],
-        data: [
-          { value: 10, name: 'unknown' },
-          { value: 15, name: 'Satisfaction OK' },
-          { value: 10, name: 'Satisfaction KO' }
-        ]
-      }
-    ]
-  };
+  private getStorySummaryById(id: string): StorySummary {
+    return this.stories.find((story) => story._id === id);
+  }
 
-  dimensionSelected() {}
+  private getStoryNameById(id: string): string {
+    return this.getStorySummaryById(id).name;
+  }
+
+  private getIndicatorByName(name: string): IndicatorDefinition {
+    return this.indicators.find((indicator) => indicator.name === name);
+  }
+
+  private getIndicatorLabelByName(name: string): string {
+    return this.getIndicatorByName(name).label;
+  }
+
+  private getIndicatorValueLabelByName(indicatorname: string, indicatorValueName: string): string {
+    return this.getIndicatorByName(indicatorname).values.find((value) => value.name === indicatorValueName).label;
+  }
+
+  get userMessagesSum() {
+    return this.messagesStatsData?.usersData?.reduce((acc, current) => acc + current[0], 0) || 0;
+  }
+
+  get answeredMessages() {
+    return this.storiesMetrics?.reduce((acc, current) => acc + current.count, 0) || 0;
+  }
+
+  get unAnsweredMessages() {
+    return this.userMessagesSum - this.answeredMessages;
+  }
+
+  get responseRate() {
+    return Math.round(((this.answeredMessages * 100) / this.userMessagesSum) * 100) / 100;
+  }
+
+  get indicatorsDimensions() {
+    return [
+      ...new Set(
+        <string>[].concat.apply(
+          [],
+          this.indicators?.map((v: IndicatorDefinition) => v.dimensions)
+        )
+      )
+    ];
+  }
+
+  setTimeRange(TimeRange: TimeRanges): void {
+    this.range.start = this.dateService.addDay(this.dateService.today(), -TimeRange);
+    this.range.end = this.dateService.today();
+
+    this.dateRangeInputDirectiveRef.writeValue(this.range);
+    this.loadMetrics();
+  }
+
+  datePickerChange(event): void {
+    if (event.end) {
+      this.loadMetrics();
+    }
+  }
 
   ngOnDestroy() {
     this.destroy.next(true);
