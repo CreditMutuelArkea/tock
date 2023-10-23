d2381f02f0dfca9c0f9e25cc581a4cf8c0fa9e9e
rkuffer <rodolphe.kuffer@partnre.com>
2023-10-23 18:19:17 +0200

    [DERCBOT-751] New rag settings

--- bot/admin/web/src/app/rag/rag-settings/models/rag-settings.ts
+++ bot/admin/web/src/app/rag/rag-settings/models/rag-settings.ts
@@ -1,26 +1,31 @@
+export enum LLMProvider {
+  OpenAI = 'OpenAI',
+  AzureOpenAIService = 'AzureOpenAIService'
+}
+
 export interface RagSettings {
   id: string;
   namespace: string;
   botId: string;
   enabled: boolean;
-  engine: string;
-  temperature: number;
-  embeddingEngine: string;
-  prompt: string;
+
   noAnswerSentence: string;
   noAnswerStoryId: string;
-  params: RagSettingsParams[];
+
+  llmSetting: llmSetting;
+  llmSettingEmbedding: llmSetting;
 }
 
-export interface RagSettingsParams {
-  apiKey?: string;
-  modelName?: string;
-  deploymentName?: string;
-  privateEndpointBaseUrl?: string;
-  apiVersion?: string;
+export interface llmSetting {
+  provider: LLMProvider;
 
-  embeddingDeploymentName?: string;
-  embeddingModelName?: string;
-  embeddingApiKey?: string;
-  embeddingApiVersion?: string;
+  temperature?: Number;
+  prompt?: String;
+
+  apiKey: String;
+  model: String;
+
+  deploymentName?: String;
+  privateEndpointBaseUrl?: String;
+  apiVersion?: String;
 }
