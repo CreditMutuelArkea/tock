7b98527bcc29601b95b4b662043993f487afefad
Mohamed ASSOUKTI <mohamed.assoukti@partnre.com>
2023-11-14 17:20:55 +0100

    [DERCBOT-753] pre-commit

--- llm/orchestrator-server/src/main/python/app/src/app/routers/healthcheck.py
+++ llm/orchestrator-server/src/main/python/app/src/app/routers/healthcheck.py
@@ -15,19 +15,19 @@
 from fastapi import APIRouter, status
 from pydantic import BaseModel
 
-router = APIRouter(prefix='/app', tags=['Health Check'])
+router = APIRouter(prefix="/app", tags=["Health Check"])
 
 
 class HealthCheck(BaseModel):
     """Response model to validate and return when performing a health check."""
 
-    status: str = 'OK'
+    status: str = "OK"
 
 
 @router.get(
-    '/health',
-    summary='Perform a Health Check',
-    response_description='Return HTTP Status Code 200 (OK)',
+    "/healthCheck",
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
     status_code=status.HTTP_200_OK,
     response_model=HealthCheck,
 )
@@ -41,4 +41,24 @@ def get_health() -> HealthCheck:
     Returns:
         HealthCheck: Returns a JSON response with the health status
     """
-    return HealthCheck(status='OK')
+    return HealthCheck(status="OK")
+
+
+@router.get(
+    "/live",
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
