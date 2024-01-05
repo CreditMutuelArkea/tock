ff0b35d3112418d3f6645cfe68d5a4faf1e51e29
Mohamed ASSOUKTI <mohamed.assoukti@partnre.com>
2024-01-05 17:58:00 +0100

    [DERCBOT-767] review wip

--- llm/orchestrator-server/src/main/python/app/src/llm_orchestrator/routers/requests/types.py
+++ llm/orchestrator-server/src/main/python/app/src/llm_orchestrator/routers/requests/types.py
@@ -1,4 +1,4 @@
-#   Copyright (C) 2023 Credit Mutuel Arkea
+#   Copyright (C) 2024 Credit Mutuel Arkea
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -16,13 +16,28 @@ from typing import Annotated, Union
 
 from fastapi import Body
 
+from llm_orchestrator.models.em.azureopenai.azure_openai_em_setting import (
+    AzureOpenAIEMSetting,
+)
+from llm_orchestrator.models.em.openai.openai_em_setting import OpenAIEMSetting
 from llm_orchestrator.models.llm.azureopenai.azure_openai_llm_setting import (
     AzureOpenAILLMSetting,
 )
 from llm_orchestrator.models.llm.openai.openai_llm_setting import (
     OpenAILLMSetting,
 )
+from llm_orchestrator.models.vector_stores.open_search.open_search_params import (
+    OpenSearchParams,
+)
 
 LLMSetting = Annotated[
     Union[OpenAILLMSetting, AzureOpenAILLMSetting], Body(discriminator='provider')
+]
+
+EMSetting = Annotated[
+    Union[OpenAIEMSetting, AzureOpenAIEMSetting], Body(discriminator='provider')
+]
+
+DocumentSearchParams = Annotated[
+    Union[OpenSearchParams], Body(discriminator='provider')
 ]
