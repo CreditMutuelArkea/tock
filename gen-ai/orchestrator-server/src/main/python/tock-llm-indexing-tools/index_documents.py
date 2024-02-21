b56734082e3e8d2c58dd283c0a26a3b4fa871a97
Pierre Guirriec <pierre.guirriec@partnre.com>
2024-02-21 14:33:04 +0100

    Prise en compte relecture PR

--- gen-ai/orchestrator-server/src/main/python/tock-llm-indexing-tools/index_documents.py
+++ gen-ai/orchestrator-server/src/main/python/tock-llm-indexing-tools/index_documents.py
@@ -49,12 +49,15 @@ import logging
 import sys
 from datetime import datetime
 from pathlib import Path
+from typing import Iterable
 from uuid import uuid4
 
 import pandas as pd
 from docopt import docopt
+from langchain.embeddings.base import Embeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import CSVLoader
+from langchain_core.documents import Document
 
 from gen_ai_orchestrator.models.em.azureopenai.azure_openai_em_setting import (
     AzureOpenAIEMSetting,
@@ -67,7 +70,6 @@ from gen_ai_orchestrator.models.em.openai.openai_em_setting import (
 from gen_ai_orchestrator.models.vector_stores.vectore_store_provider import (
     VectorStoreProvider,
 )
-from gen_ai_orchestrator.routers.requests.types import EMSetting
 from gen_ai_orchestrator.services.langchain.factories.langchain_factory import (
     get_em_factory,
     get_vector_store_factory,
@@ -116,7 +118,38 @@ def index_documents(args):
         chunk_size=int(args['<chunks_size>'])
     )
     splitted_docs = text_splitter.split_documents(docs)
-    # Add chunk id to each chunk
+    # Add chunk id ('n/N') metadata to each chunk
+    splitted_docs = generate_ids_for_each_chunks(splitted_docs=splitted_docs)
+
+    logging.debug(f"Get embeddings model from {args['<embeddings_cfg>']} config file")
+    with open(args['<embeddings_cfg>'], 'r') as file:
+        config_dict = json.load(file)
+    em_settings = em_settings_from_config(config_dict)
+
+    # Use embeddings factory from orchestrator
+    em_factory = get_em_factory(em_settings)
+    em_factory.check_embedding_model_setting()
+    embeddings = em_factory.get_embedding_model()
+
+    # Index all chunks in vector DB
+    embed_and_store_docs(
+        splitted_docs, embeddings=embeddings, index_name=args['<index_name>']
+    )
+
+    # Print statistics
+    duration = datetime.now() - start_time
+    logging.debug(
+        f"Indexed {len(splitted_docs)} chunks in '{args['<index_name>']}' from {len(docs)} lines in '{args['<input_csv>']}' (duration: {duration})"
+    )
+
+    # Return session index uuid to main script
+    return session_uuid
+
+
+def generate_ids_for_each_chunks(
+    splitted_docs: Iterable[Document],
+) -> Iterable[Document]:
+    """Add chunk id ('n/N') to the documents' metadata using Pandas."""
     metadata = [doc.metadata for doc in splitted_docs]
     df_metadata = pd.DataFrame(metadata)
     df_metadata['total_chunks'] = df_metadata.groupby('id')['id'].transform('count')
@@ -128,45 +161,39 @@ def index_documents(args):
     )
     for i, doc in enumerate(splitted_docs):
         doc.metadata['chunk'] = df_metadata.loc[i, 'chunk']
+    return splitted_docs
 
-    logging.debug(f"Get embeddings model from {args['<embeddings_cfg>']} config file")
-    with open(args['<embeddings_cfg>'], 'r') as file:
-        config_dict = json.load(file)
+
+def em_settings_from_config(setting_dict: dict) -> BaseEMSetting:
+    """Get embeddings settings from config dict."""
     # Create settings class according to embeddings provider from config file
     if config_dict['provider'] == EMProvider.OPEN_AI:
-        em_settings = OpenAIEMSetting(**config_dict)
+        em_settings = OpenAIEMSetting(**setting_dict)
     elif config_dict['provider'] == EMProvider.AZURE_OPEN_AI_SERVICE:
-        em_settings = AzureOpenAIEMSetting(**config_dict)
+        em_settings = AzureOpenAIEMSetting(**setting_dict)
 
-    # Use embeddings factory from orchestrator
-    em_factory = get_em_factory(em_settings)
-    em_factory.check_embedding_model_setting()
-    embeddings = em_factory.get_embedding_model()
+    return em_settings
 
+
+def embed_and_store_docs(
+    documents: Iterable[Document], embeddings: Embeddings, index_name: str
+) -> None:
+    """ "Embed all chunks in vector database."""
     logging.debug('Index chunks in DB')
     # Use vector store factory from orchestrator
     vectorstore_factory = get_vector_store_factory(
         VectorStoreProvider.OPEN_SEARCH,
         embedding_function=embeddings,
-        index_name=args['<index_name>'],
+        index_name=index_name,
     )
     opensearch_db = vectorstore_factory.get_vector_store()
     # Index respecting bulk_size (500 is from_documents current default: it is described for clarity only)
     bulk_size = 500
-    for i in range(0, len(splitted_docs), bulk_size):
-        logging.debug(f'i={i}, splitted_docs={len(splitted_docs)}')
+    for i in range(0, len(documents), bulk_size):
+        logging.debug(f'i={i}, splitted_docs={len(documents)}')
         opensearch_db.add_documents(
-            documents=splitted_docs[i : i + 500], bulk_size=bulk_size
+            documents=documents[i : i + 500], bulk_size=bulk_size
         )
-
-    # Print statistics
-    duration = datetime.now() - start_time
-    logging.debug(
-        f"Indexed {len(splitted_docs)} chunks in '{args['<index_name>']}' from {len(docs)} lines in '{args['<input_csv>']}' (duration: {duration})"
-    )
-
-    # Return session index uuid to main script
-    return session_uuid
 
 
 def index_name_is_valid(index_name: str) -> bool:
