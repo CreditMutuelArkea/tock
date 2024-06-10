a8f40613bc65250f527923f67b09d86322481522
Mohamed ASSOUKTI <mohamed.assoukti@partnre.com>
2024-06-10 16:26:11 +0200

    [DERCBOT-1028] WIP - Test OK

---
+++ gen-ai/orchestrator-server/src/main/python/server/src/gen_ai_orchestrator/services/langchain/factories/callback_handlers/langfuse_callback_handler_factory.py
@@ -0,0 +1,39 @@
+#   Copyright (C) 2023-2024 Credit Mutuel Arkea
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+"""Model for creating Langfuse Callback Handler Factory"""
+
+from langfuse.callback import CallbackHandler as LangfuseCallbackHandler
+
+from gen_ai_orchestrator.models.observability.observability_type import ObservabilitySetting
+from gen_ai_orchestrator.services.langchain.factories.callback_handlers.callback_handlers_factory import \
+    LangChainCallbackHandlerFactory
+from gen_ai_orchestrator.services.security.security_service import fetch_secret_key_value
+
+
+class LangfuseCallbackHandlerFactory(LangChainCallbackHandlerFactory):
+    """A class for Langfuse Callback Handler Factory"""
+
+    setting: ObservabilitySetting
+
+    def get_callback_handler(self) -> LangfuseCallbackHandler:
+        return LangfuseCallbackHandler(
+            host=str(self.setting.url),
+            public_key=self.setting.public_key,
+            secret_key=fetch_secret_key_value(self.setting.secret_key),
+        )
+
+    def check_setting(self) -> bool:
+        self.get_callback_handler().auth_check()
+        return True
