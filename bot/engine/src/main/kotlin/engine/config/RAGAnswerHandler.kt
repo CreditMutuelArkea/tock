0a0af39759fa9d6901543c72cec5d5f298e1caa2
Mohamed ASSOUKTI <mohamed.assoukti@partnre.com>
2024-06-12 15:59:16 +0200

    [DERCBOT-1034] Use of observability on Gen AI services

--- bot/engine/src/main/kotlin/engine/config/RAGAnswerHandler.kt
+++ bot/engine/src/main/kotlin/engine/config/RAGAnswerHandler.kt
@@ -35,6 +35,7 @@ import ai.tock.genai.orchestratorclient.responses.TextWithFootnotes
 import ai.tock.genai.orchestratorclient.retrofit.GenAIOrchestratorBusinessError
 import ai.tock.genai.orchestratorclient.retrofit.GenAIOrchestratorValidationError
 import ai.tock.genai.orchestratorclient.services.RAGService
+import ai.tock.genai.orchestratorcore.mappers.ObservabilitySettingMapper
 import ai.tock.genai.orchestratorcore.utils.OpenSearchUtils
 import ai.tock.shared.*
 import engine.config.AbstractProactiveAnswerHandler
@@ -176,6 +177,7 @@ object RAGAnswerHandler : AbstractProactiveAnswerHandler {
                                 Term(term = mapOf("metadata.index_session_id.keyword" to ragConfiguration.indexSessionId!!))
                             )
                         ),
+                        observabilitySetting = botDefinition.observabilityConfiguration?.setting
                     ), debug = action.metadata.debugEnabled || ragDebugEnabled
                 )
 
